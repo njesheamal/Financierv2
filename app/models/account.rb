6529b33193d8e6302817759ee047f50994ef5487
abduamal <abduamal@ymail.com>
2021-03-05 09:55:27 -0600

    account validation update

--- app/models/account.rb
+++ app/models/account.rb
@@ -6,5 +6,5 @@ class Account < ApplicationRecord
   validates :name, uniqueness: {message: "Nickname must be unique"}
   validates :account_number, uniqueness: {message: "Account number must be unique"}
   validates :credit_limit, presence: {message: "Please enter your credit limit"}
-  validates_length_of :account_number, within: 9..16, on: :create, message: "Must be at least 9 digits"
+  validates_length_of :account_number, within: 9..16, on: :create, message: "Must be between 9-16 digits"
 end
