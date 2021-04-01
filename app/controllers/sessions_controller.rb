dd01aa168e58433f8bc013fe3c699028e3719689
abduamal <abduamal@ymail.com>
2021-04-01 12:39:06 -0500

    Clean-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by_email(params[:email])
         #if the user exists AND the password is correct:
-        if @user && user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             #save the user id in the browsers cookies
             #this keeps the user logged in as they navigate the app
             session[:user_id] = @user.id
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,8 +5,7 @@ class Transaction < ApplicationRecord
     has_many :payments, through: :transactions_payment
 
     scope :biggest_purchase, -> {where(amount: self.maximum(:amount))}
-
-
+    # scope :check_payment, -> (amount) { joins(:payment).merge(Payment.paid) => amount}
     # scope :transaction_categories, -> { joins(:category).merge(Category.order(priority: :desc))}
     validates :amount, presence: {message: "Must have an amount"}
 end
