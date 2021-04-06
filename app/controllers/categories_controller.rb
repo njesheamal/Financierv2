52bd8611cd42692cc73635a3871cbcb5b9570ee7
abduamal <abduamal@ymail.com>
2021-04-06 15:39:39 -0500

    Add Categories actions and scope

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,21 @@
 class CategoriesController < ApplicationController
+  def index
+    @category = Category.all
+  end
+
+  def show
+    find_account
+    find_category
+  end
+  
+  
+  private
+
+  def find_category
+    @category = Category.find(params[:category_id])
+  end
+  
+  def find_account
+    @account = Account.find(params[:account_id])
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ApplicationRecord
     has_many :transactions
     has_many :accounts, through: :transactions
     
-    
+    # scope :most_categories, -> { joins(:transaction.category_id).where(category_id => )}
+
+    scope :category_title, -> (digit) { where(:category_id => digit)}
 end 
