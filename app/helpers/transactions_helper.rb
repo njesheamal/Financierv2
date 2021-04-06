050848d03d1f1793b4bff5b992b3eb0875a26044
abduamal <abduamal@ymail.com>
2021-04-06 11:53:22 -0500

    format date for Transaction

--- app/helpers/transactions_helper.rb
+++ app/helpers/transactions_helper.rb
@@ -1,5 +1,9 @@
 module TransactionsHelper
-    def format_date
-        
+    def format_date(transaction)
+        if transaction.date == nil
+            transaction.date = transaction.created_at.to_date
+        else
+            transaction.date
+        end
     end
 end
